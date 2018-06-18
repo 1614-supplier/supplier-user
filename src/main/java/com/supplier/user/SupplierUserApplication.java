d45465414dd11d33ec2a85c4d7a5f81fda5d9b0d
zhenhai.zheng <zhenhai.zheng@foxmail.com>
2018-06-18 21:13:21 +0800

    install

---
+++ src/main/java/com/supplier/user/SupplierUserApplication.java
@@ -0,0 +1,12 @@
+package com.supplier.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierUserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/supplier/user/SupplierUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.supplier.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupplierUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
