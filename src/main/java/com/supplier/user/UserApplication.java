db4e1851c582a39f1d34c8ade2a626c0e53f441a
zhenhai.zheng <zhenhai.zheng@foxmail.com>
2019-02-12 16:45:13 +0800

    初始化项目

--- src/main/java/com/supplier/user/UserApplication.java
+++ src/main/java/com/supplier/user/UserApplication.java
@@ -2,8 +2,12 @@ package com.supplier.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableEurekaClient
 public class UserApplication {
 
 	public static void main(String[] args) {
